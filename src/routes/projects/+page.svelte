5298521bba13b40f84a631a28c31dbbcd4b6a751
Moody Rahman <moody.byte@gmail.com>
2024-05-18 21:40:28 -0400

    projs

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,8 +1,17 @@
-<h5><a href="./1" class="highlight">Project 1</a></h5>
+<!-- <h5><a href="./1" class="highlight">Project 1</a></h5>
 <h5><a href="./2" class="highlight">Project 2</a></h5>
 <h5><a href="./3" class="highlight">Project 3</a></h5>
 <h5><a href="./4" class="highlight">Project 4</a></h5>
 <h5><a href="./5" class="highlight">Project 5</a></h5>
 <h5><a href="./6" class="highlight">Project 6</a></h5>
 <h5><a href="./7" class="highlight">Project 7</a></h5>
+<h5><a href="../" class="highlight">../</a></h5> -->
+
+<h5><a href="#" class="highlight">Project 1</a></h5>
+<h5><a href="#" class="highlight">Project 2</a></h5>
+<h5><a href="#" class="highlight">Project 3</a></h5>
+<h5><a href="#" class="highlight">Project 4</a></h5>
+<h5><a href="#" class="highlight">Project 5</a></h5>
+<h5><a href="#" class="highlight">Project 6</a></h5>
+<h5><a href="#" class="highlight">Project 7</a></h5>
 <h5><a href="../" class="highlight">../</a></h5>
