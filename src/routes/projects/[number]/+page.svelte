8c05516924c0c94a87602d04e66ed2cf299e29bd
Moody Rahman <moody.byte@gmail.com>
2024-06-12 10:07:39 -0400

    better colors

--- src/routes/projects/[number]/+page.svelte
+++ src/routes/projects/[number]/+page.svelte
@@ -10,4 +10,14 @@
 <h5>
   <a href="../" class="highlight">../</a>
 </h5>
+
 <!-- {data.files} -->
+
+<style>
+  :global(pre) {
+    background-color: #666661;
+    padding: 5px;
+    color: #cdcdc1;
+    border-radius: 5px;
+  }
+</style>
